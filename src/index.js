ff9cb0b8e74f278e0c8c14838943a17473a38788
am1macdonald <am1macdonald@gmail.com>
2022-02-23 17:00:11 -0500

    general commit. getting api call functions figured out.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import reset from "./stylesheets/reset.css"
-import sass from "./stylesheets/style.sass"
+import './stylesheets/reset.css';
+import './stylesheets/style.sass';
+import {
+  fetchCoordinates, fetchWeather, getUserInput, location,
+} from './scripts/dataHandler';
+
+const getWeather = async () => {
+  const place = getUserInput();
+  console.log(place);
+
+  const geoCoords = await location();
+
+  console.log(geoCoords);
+  const coords = await fetchCoordinates(place);
+
+  const lat = String(coords.lat);
+  const lon = String(coords.lon);
+
+  console.log(lat, lon);
+  console.log(fetchWeather(lat, lon));
+};
+
+getWeather();
